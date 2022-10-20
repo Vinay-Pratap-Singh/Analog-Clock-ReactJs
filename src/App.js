759ab6e4c08987cbed16f740d23dbbf1409c36d5
Vinay Pratap Singh <technicalharvi@gmail.com>
2022-10-20 11:58:40 +0530

    updated clock ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import clock from "./clock.png";
 const App = () => {
   // using the useState to update the hour, minute and second
   const [hrDegree, setHrDegree] = useState("0");
@@ -27,9 +26,9 @@ const App = () => {
 
   return (
     // creating a wrapper div to wrpa whole code inside that
-    <div className="w-full h-screen flex items-center justify-center">
+    <div className="w-full h-screen flex items-center justify-center bg-gray-800">
       {/* creating the clock div */}
-      <div className="clock w-96 h-96 rounded-[50%] border flex items-center justify-center relative">
+      <div className="clock w-96 h-96 rounded-[50%] border flex items-center justify-center relative bg-gray-500 boxShadow">
         {/* div for hour hand */}
         <div style={{transform:`rotate(${hrDegree}deg)`}} className="bg-red-500 h-16 w-1 absolute z-10 origin-bottom top-[33%] rounded-[50%]"></div>
 
