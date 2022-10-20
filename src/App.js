333a3375a18fe582ba86f8e28520170baf76faa3
Vinay Pratap Singh <technicalharvi@gmail.com>
2022-10-20 09:46:22 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import clock from "./clock.png";
+const App = () => {
+  // using the useState to update the hour, minute and second
+  const [hrDegree, setHrDegree] = useState("0");
+  const [minDegree, setMinDegree] = useState("0");
+  const [secDegree, setSecDegree] = useState("0");
+  // set interval to update the real time data in the dom
+  useEffect(() => {
+    const interval = setInterval(() => {
+      let date = new Date();
+      let hr = date.getHours();
+      let min = date.getMinutes();
+      let sec = date.getSeconds();
 
-function App() {
+      let hrRotation = hr * 30 + min / 2;
+      let minRotation = min * 6;
+      let secRotation = sec * 6;
+
+      setHrDegree(hrRotation);
+      setMinDegree(minRotation);
+      setSecDegree(secRotation);
+    }, 1000);
+  
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // creating a wrapper div to wrpa whole code inside that
+    <div className="w-full h-screen flex items-center justify-center">
+      {/* creating the clock div */}
+      <div className="clock w-96 h-96 rounded-[50%] border flex items-center justify-center relative">
+        {/* div for hour hand */}
+        <div style={{transform:`rotate(${hrDegree}deg)`}} className="bg-red-500 h-16 w-1 absolute z-10 origin-bottom top-[33%] rounded-[50%]"></div>
+
+        {/* div for minute hand */}
+        <div style={{transform:`rotate(${minDegree}deg)`}} className="bg-blue-500 h-[5rem] w-1 absolute z-20 origin-bottom top-[29%] rounded-[50%]"></div>
+
+        {/* div for second hand */}
+        <div style={{transform:`rotate(${secDegree}deg)`}} className=" bg-green-500 h-24 w-1 absolute z-30 origin-bottom top-[25%] rounded-[50%]"></div>
+
+        {/* div for circular dot in center of clock */}
+        <div className="w-4 h-4 bg-black rounded-[50%] absolute z-40"></div>
+      </div>
     </div>
   );
 }
